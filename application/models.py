d208df4eb888203a118611fa5afd753f0fb797dd
AKekitinisa <kkekitinisa@gmail.com>
2020-07-31 12:43:47 +0000

    adding relationship to tables

--- application/models.py
+++ application/models.py
@@ -5,13 +5,13 @@ from flask_login import UserMixin
 
 
 class Events(db.Model):
-    __tablename__='event'
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__='events'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     event_name = db.Column(db.String(30), nullable=False)
     event_date = db.Column(db.DateTime, nullable=False)
     location = db.Column(db.String(100), nullable=False, unique=True)
     description = db.Column(db.String(500), nullable=False, unique=True)
-    event_details = db.relationship('EventsDetails', backref='event', lazy=True)
+    event_details = db.relationship('EventsDEtails', backref='event',primaryjoin='events.id==event_details.event_id', lazy='dynamic')
 
     def __repr__(self):
         return ''.join([
@@ -25,11 +25,11 @@ def load_user(id):
     return Users.query.get(int(id))
         
 class Users(db.Model, UserMixin):
-    __tablename__='user'
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     email = db.Column(db.String(500), nullable=False, unique=True)
     password = db.Column(db.String(500), nullable=False)
-    event_details = db.relationship('EventsDetails', backref='user', lazy=True)
+    event_details = db.relationship('EventDetails', backref='user',primaryjoin='users.id==event_details.user_id' ,lazy='dynamic')
     
     def __repr__(self):
         return ''.join([
@@ -40,9 +40,9 @@ class Users(db.Model, UserMixin):
 
 class EventDetails(db.Model):
         __tablename__='event_details'
-        id = db.Column(db.Integer,primary_key=True)
-        user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-        event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+        id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+        user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+        event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
 
         def __repr__(self):
             return '<Event Details: {}>'.format(self.id)
